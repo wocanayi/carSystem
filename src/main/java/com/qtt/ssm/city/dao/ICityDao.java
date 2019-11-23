aac0810b39cb4036b2f410ddaba83428f9d17996
wocanayi <993519925@qq.com>
2019-11-23 16:29:23 +0800

    添加了注释。

--- src/main/java/com/qtt/ssm/city/dao/ICityDao.java
+++ src/main/java/com/qtt/ssm/city/dao/ICityDao.java
@@ -10,9 +10,26 @@ import java.util.List;
  */
 @Repository
 public interface ICityDao {
+    /**
+     * 根据pid选择city
+     *
+     * @param pid pid
+     * @return city列表
+     */
     List<City> selectByPid(Integer pid);
 
+    /**
+     * 选择所有的city
+     *
+     * @return city列表
+     */
     List<City> selectAll();
 
+    /**
+     * 根据id选择city
+     *
+     * @param id id
+     * @return city
+     */
     City selectByPrimaryKey(Integer id);
 }
--- src/main/java/com/qtt/ssm/city/service/ICityService.java
+++ src/main/java/com/qtt/ssm/city/service/ICityService.java
@@ -9,9 +9,27 @@ import java.util.List;
  * @author XC
  */
 public interface ICityService {
+    /**
+     * 根据pid选择city
+     *
+     * @param pid pid
+     * @return city列表
+     */
     List<City> selectAll(Integer pid);
 
+    /**
+     * 选择所有的city
+     *
+     * @return city列表
+     */
     List<City> selectAllInfo();
 
+    /**
+     * 根据id选择city
+     *
+     * @param getid 取车id
+     * @param backid 还车id
+     * @return city的name
+     */
     VCity selectCity(Integer getid, Integer backid);
 }
